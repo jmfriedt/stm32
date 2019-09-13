5ac13fe4f386bff7ed2fec0c151790a314970ae5
jmfriedt <jean-michel.friedt@femto-st.fr>
2019-09-13 19:46:08 +0200

    clock source

--- uart_f4.c
+++ uart_f4.c
@@ -13,9 +13,12 @@ const struct rcc_clock_scale rcc_hse_20mhz_3v3 = {
                 .plln = 280,                          // 1*280/2=140 MHz
                 .pllp = 2,                            //       ^
                 .pllq = 6,
+		.pllr=0,
+		.pll_source = RCC_CFGR_PLLSRC_HSE_CLK, // 190913 mandatory to use external clock (!=HSI)
                 .hpre = RCC_CFGR_HPRE_DIV_NONE,
                 .ppre1 = RCC_CFGR_PPRE_DIV_4,
                 .ppre2 = RCC_CFGR_PPRE_DIV_2,
+		.voltage_scale = PWR_SCALE1,
                 .flash_config = FLASH_ACR_ICEN | FLASH_ACR_DCEN |
                                 FLASH_ACR_LATENCY_4WS, // 4 WS d'apres configuration par ST
                 .ahb_frequency  = 140000000,  
