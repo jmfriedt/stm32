b208fcaecf9a6343099d48c8bf1c9c65e1837e16
jfriedt <jean-michel.friedt@femto-st.fr>
2020-09-09 11:12:00 +0200

    delay

--- main_uart.c
+++ main_uart.c
@@ -1,6 +1,7 @@
 // STM32F4Disco: DO NOT use USART1 which is also used by USB OTG
 // http://www.micromouseonline.com/2013/05/05/using-usart1-on-the-stm32f4discovery/
 
+#define usart1
 #include "uart.h"
 
 int main()
@@ -14,6 +15,7 @@ int main()
    mon_putchar('r');
    mon_putchar('\r');
    mon_putchar('\n');
+   delay(0xffff);
   }
  return 0;
 }
